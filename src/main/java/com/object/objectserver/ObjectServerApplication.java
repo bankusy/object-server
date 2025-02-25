bd061fa2708888a040da66e8d04cb44256e331f8
bankusy <dlwoddu821@gmail.com>
2025-02-25 11:32:48 +0900

    init: 패키지 초기화

---
+++ src/main/java/com/object/objectserver/ObjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.object.objectserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/object/objectserver/ObjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.object.objectserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
